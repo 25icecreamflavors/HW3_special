c78c43549b52468e387573385970ca14b22980dd
Alexander <alexamailwork@gmail.com>
2022-12-21 11:18:49 +0300

    Added argparse for predictions

--- entrypoint.py
+++ entrypoint.py
@@ -1,5 +1,14 @@
 from models.create_model import predict
 from conf.conf import logging, settings
+import argparse
 
-pred = predict(settings.PREDICT.test, model=settings.MODEL.gb)
+# Creating argparse to run predictions from the command line
+parser = argparse.ArgumentParser(description='Args for models predictions')
+parser.add_argument('params', type=str, help='Sample params to get predictions')
+args = parser.parse_args()
+
+# Create the list from enetered prediction params
+prediction_params = [[int(item) for item in args.params.split(',')]]
+
+pred = predict(prediction_params, model=settings.MODEL.gb)
 logging.info(f"Prediction - {pred}")
